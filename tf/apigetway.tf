40fe16d4c7a3177bbbf0218d3464b5efa071685b
mikedido <mahdigueffaz@gmail.com>
2022-05-30 23:12:46 +0200

    Correct the tf

--- tf/apigetway.tf
+++ tf/apigetway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "apiSacem" {
     description   = "Terraform Serverless Application Example"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+/*resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.apiSacem.id}"
   parent_id   = "${aws_api_gateway_rest_api.apiSacem.root_resource_id}"
   path_part   = "{proxy+}"
@@ -16,41 +16,65 @@ resource "aws_api_gateway_method" "proxyMethod" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda1" {
-  rest_api_id = "${aws_api_gateway_rest_api.apiSacem.id}"
-  resource_id = "${aws_api_gateway_method.proxyMethod.resource_id}"
-  http_method = "${aws_api_gateway_method.proxyMethod.http_method}"
-
-  integration_http_method = "GET"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.sacem_get_files.invoke_arn}"
-}
-
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id   = "${aws_api_gateway_rest_api.apiSacem.id}"
   resource_id   = "${aws_api_gateway_rest_api.apiSacem.root_resource_id}"
   http_method   = "ANY"
   authorization = "NONE"
+}*/
+
+resource "aws_api_gateway_resource" "sacem_files" {
+  rest_api_id = "${aws_api_gateway_rest_api.apiSacem.id}"
+  parent_id   = "${aws_api_gateway_rest_api.apiSacem.root_resource_id}"
+  path_part   = "files"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
+resource "aws_api_gateway_method" "sacem_get_files" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apiSacem.id}"
+  resource_id   = "${aws_api_gateway_resource.sacem_files.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "sacem_put_file" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apiSacem.id}"
+  resource_id   = "${aws_api_gateway_resource.sacem_files.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda1" {
    rest_api_id = aws_api_gateway_rest_api.apiSacem.id
-   resource_id = aws_api_gateway_method.proxy_root.resource_id
-   http_method = aws_api_gateway_method.proxy_root.http_method
+   resource_id = aws_api_gateway_resource.sacem_files.id
+   http_method = aws_api_gateway_method.sacem_get_files.http_method
 
    integration_http_method = "POST"
    type                    = "AWS_PROXY"
    uri                     = aws_lambda_function.sacem_get_files.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "lambda2" {
+  rest_api_id = "${aws_api_gateway_rest_api.apiSacem.id}"
+  resource_id = "${aws_api_gateway_resource.sacem_files.id}"
+  http_method = "${aws_api_gateway_method.sacem_put_file.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.sacem_put_files.invoke_arn}"
+}
+
 resource "aws_api_gateway_deployment" "apideploy" {
   depends_on = [
     aws_api_gateway_integration.lambda1,
-    aws_api_gateway_integration.lambda_root
+    aws_api_gateway_integration.lambda2
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.apiSacem.id}"
-  stage_name  = "test"
+  stage_name  = "api"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 
--- tf/lambdas.tf
+++ tf/lambdas.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "sacem_get_files" {
     s3_key           = aws_s3_object.sacem_get_files.key
 
     runtime          = "python3.8"
-    handler          = "lambda_function.lambda_handler"
+    handler          = "main.lambda_handler"
     timeout          = 10
 
     source_code_hash = data.archive_file.lambda_sacem_get_files_package.output_base64sha256
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "sacem_put_files" {
     s3_key           = aws_s3_object.sacem_put_files.key
 
     runtime          = "python3.8"
-    handler          = "lambda_function.lambda_handler"
+    handler          = "main.lambda_handler"
     timeout          = 10
 
     source_code_hash = data.archive_file.lambda_sacem_put_files_package.output_base64sha256
